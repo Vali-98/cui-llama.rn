083539a1aad550d16564a3356a46c377eee42517
Vali98 <al244098@gmail.com>
2025-01-28 18:03:25 +0800

    fix: build on windows

--- scripts/build-android.sh
+++ scripts/build-android.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 NDK_VERSION=26.3.11579264
-CMAKE_TOOLCHAIN_FILE=$ANDROID_HOME/ndk/$NDK_VERSION/build/cmake/android.toolchain.cmake
+CMAKE_TOOLCHAIN_FILE="${ANDROID_HOME//\\//}/ndk/$NDK_VERSION/build/cmake/android.toolchain.cmake"
 ANDROID_PLATFORM=android-21
 CMAKE_BUILD_TYPE=Release
 
@@ -22,7 +22,7 @@ fi
 cd android/src/main
 
 # Build the Android library (arm64-v8a)
-cmake -DCMAKE_TOOLCHAIN_FILE=$CMAKE_TOOLCHAIN_FILE \
+cmake -G "Unix Makefiles" -DCMAKE_TOOLCHAIN_FILE="$CMAKE_TOOLCHAIN_FILE" \
   -DANDROID_ABI=arm64-v8a \
   -DANDROID_PLATFORM=$ANDROID_PLATFORM \
   -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE \
@@ -38,7 +38,7 @@ cp build-arm64/*.so jniLibs/arm64-v8a/
 rm -rf build-arm64
 
 # Build the Android library (x86_64)
-cmake -DCMAKE_TOOLCHAIN_FILE=$CMAKE_TOOLCHAIN_FILE \
+cmake -G "Unix Makefiles" -DCMAKE_TOOLCHAIN_FILE=$CMAKE_TOOLCHAIN_FILE \
   -DANDROID_ABI=x86_64 \
   -DANDROID_PLATFORM=$ANDROID_PLATFORM \
   -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE \
